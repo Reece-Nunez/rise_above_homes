33c1c7ea3db8fe295b8ed5596bd9d34b36a4cc85
Reece Nunez <reecenunez@hotmail.com>
2024-01-25 09:47:50 -0600

    updated package.json and other files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteFaviconsPlugin from 'favicons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/rise_above_homes',
+  plugins: [
+    react(),
+    ViteFaviconsPlugin('./src/assets/transparent_black_just_logo.svg')
+  ],
 })
