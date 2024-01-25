1435d697af14378e33bcaa78886dfb8bdf81f8df
Reece Nunez <reecenunez@hotmail.com>
2024-01-25 10:48:44 -0600

    removed favicons

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteFaviconsPlugin from 'favicons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/rise_above_homes',
   plugins: [
     react(),
-    ViteFaviconsPlugin('./src/assets/transparent_black_just_logo.svg')
   ],
 })
